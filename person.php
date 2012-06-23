cc429f85310b826d4c9a7d4c3387c1583e0e03d3
Jash Patel <jashpp@gmail.com>
2012-06-23 02:51:09 -0400

    Update person.php to use Jquery mobile

--- person.php
+++ person.php
@@ -1,186 +1,111 @@
 <?php
-include_once('php-sdk/facebook.php');
-			date_default_timezone_set('UTC');
-			if (isset($_REQUEST['id'])) {
-			  $b['id'] = $_REQUEST['id'];
-			}
-
-			$app_url = "https://csclub.uwaterloo.ca/~y5pei"; // no slash at the end, e.g. 'https://social-cafe.herokuapp.com'
-			$app_id = "152916161509664";
-			$app_secret = "673c3da0f16c3a6fd357dda1e6cdfffc";
-			$app_namespace = "hbd-fbhack"; // no colon at the end, e.g. 'social-cafe'
-
-
-			$facebook = new Facebook( array(
-									   'appId' => $app_id,
-									   'secret' => $app_secret,
-									 ));
-			  
-			  $login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_actions,user_birthday,user_likes,friends_birthday,friends_relationships,friends_likes,publish_stream') );
-
-  
-			function getCommon($personId, $connections){
-				
-			  global $facebook;
-			  $common = array();
-				$them = $facebook->api("/$personId/$connections", 'GET');
-				$me = $facebook->api("/me/$connections", 'GET');
-
-				foreach($them['data'] as $t) {
-					foreach($me['data'] as $m){
-						if ($t['id'] == $m['id']) {
-							$tmp = array();
-							$tmp['id'] = $t['id'];
-				  			$tmp['name'] = $t['name'];
-				  			$common[] = $tmp;
-						}
-					}
-				}
-
-			return $common;
-			}
-			$groups = getCommon($b['id'], "groups");
-			$music = getCommon($b['id'], "music");
-			$books = getCommon($b['id'], "books");
-			$games = getCommon($b['id'], "games");
-			$movies = getCommon($b['id'], "movies");
-			$television = getCommon($b['id'], "television");
-			$events = getCommon($b['id'], "events");
+	include_once('config.php');
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
+	} 
 	
-	if (!empty($movies) || !empty($music) || !empty($groups) || !empty($books) || !empty($games) || !empty($television) || !empty($events))
-		echo '<p> This person has something in common with you! Wish them a happy birthday!!</p>';
-	else 
-		echo '<p> Darn! You have nothing in common (on Facebook)...say something anyway?</p>'; 
+	function getCommon(&$facebook, $personId, $connections){
+		$common = array();
+		$them = $facebook->api("/$personId/$connections", 'GET');
+		$me = $facebook->api("/me/$connections", 'GET');
 
-	if (!empty($movies)) {
-		echo '<h1>Movies</h1>';
-		foreach ($movies as $g) { 
-			echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            echo '<form method="post" action="message.php?id='.$b['id'].'&element_id='.$g['id'].'">';
-            echo '<button class="btn">Do it!</button>';
-            echo '</form>';
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-		}	
+		foreach($them['data'] as $t) {
+			foreach($me['data'] as $m){
+				if ($t['id'] == $m['id']) {
+					$common[] = $t;
+				}
+			}
+		}
+		return $common;
 	}
-
-	if (!empty($music)) {
-		echo '<h1>Music</h1>';
-		foreach ($music as $g) { 
-			echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            // <form method= "post" action="person.php? echo 'id='.$b['id'];">
-            echo '<button class="btn">Do it!</button>';
-            //</form>
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-
-		}	
-	}
-
-	if (!empty($groups)) {
-		echo '<h1>Groups</h1>';
-		foreach ($groups as $g) { 
-			echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            // <form method= "post" action="person.php? echo 'id='.$b['id'];">
-            echo '<button class="btn">Do it!</button>';
-            //</form>
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-
-		}	
-	}
-
-	if (!empty($books)) {
-		echo '<h1>Books</h1>';
-		foreach ($books as $g) { 
-			echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            // <form method= "post" action="person.php? echo 'id='.$b['id'];">
-            echo '<button class="btn">Do it!</button>';
-            //</form>
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-
-		}	
-	}
-
-	if (!empty($games)) {
-		echo '<h1>Games</h1>';
-		foreach ($games as $g) { 
-			echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            // <form method= "post" action="person.php? echo 'id='.$b['id'];">
-            echo '<button class="btn">Do it!</button>';
-            //</form>
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-
-		}	
-	}
-
-	if (!empty($television)) {
-		echo '<h1>TV</h1>';
-		foreach ($television as $g) { 
-		echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            // <form method= "post" action="person.php? echo 'id='.$b['id'];">
-            echo '<button class="btn">Do it!</button>';
-            //</form>
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-
-		}	
-	}
-
-	if (!empty($events)) {
-		echo '<h1>Events</h1>';
-		foreach ($events as $g) { 
-	echo '<div class="row">';
-			echo '<div class="span4">';
-			echo $g['name'];
-			echo '</div>';
-			echo '<div class="span8">';
-			echo '<div class="btn-group">';
-            // <form method= "post" action="person.php? echo 'id='.$b['id'];">
-            echo '<button class="btn">Do it!</button>';
-            //</form>
-          echo'</div>';
-          echo'</div>';
-echo'</div>';
-
-		}	
+	
+	$isLoggedIn = $facebook->getUser() && isset($id);
+	if($isLoggedIn) {
+		$interestNames = array("groups", "music", "books", "games", "movies", "television", "events");
+		foreach($interestNames as $interestName){
+			$interests[$interestName] = getCommon($facebook, $id, $interestName);
+		}
 	}
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>
+        </title>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />
+        <link rel="stylesheet" href="css/local.css" />
+        <style>
+            /* App custom styles */
+        </style>
+		<script type="text/javascript">
+			//Fixes facebook/jquery bug
+			if (window.location.hash == '#_=_')window.location.hash = '';
+		</script>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+        </script>
+        <script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js">
+        </script>
+    </head>
+    <body>
+        <!-- Home -->
+		<div data-role="page" id="page1">
+            <div data-theme="a" data-role="header">
+                <h3>
+                    Hacky Birthday!
+                </h3>
+            </div>
+            <div data-role="content" style="padding: 15px">
+				<?php if($isLoggedIn) { ?>
+					<div data-role="fieldcontain">
+						<fieldset data-role="controlgroup" data-type="horizontal">
+							<legend>
+								Whose likes do you want to show?
+							</legend>
+							<input name="radiobuttons1" id="radio2" value="radio2" type="radio" />
+							<label for="radio2">
+								Thier's
+							</label>
+							<input name="radiobuttons1" id="radio1" value="radio1" type="radio" />
+							<label for="radio1">
+								Common
+							</label>
+							<input name="radiobuttons1" id="radio3" value="radio3" type="radio" />
+							<label for="radio3">
+								Your's
+							</label>
+						</fieldset>
+					</div>
+					<ul data-role="listview" data-divider-theme="a" data-inset="true">
+						<?php 
+						foreach ($interests as $interestName => $interest) {
+							if (!empty($interest)) {
+						?>
+								<li data-role="list-divider" role="heading">
+									<?php print ucfirst($interestName); ?>
+								</li>
+								<?php foreach ($interest as $item) { ?>
+									<li data-theme="c">
+										<a href="<?php print "message.php?id=".$id."&item=".$item['id']; ?>" data-transition="slide">
+											<?php print $item['name']; ?>
+										</a>
+									</li>
+								<?php }
+							}
+						} ?>
+					</ul>
+				</div>
+				<?php } else { ?>
+					<a data-role="button" data-transition="fade" href="<?php print $login_url; ?>" data-icon="plus" data-iconpos="left">
+						Login with Facebook
+					</a>
+				<?php } ?>
+            </div>
+        </div>
+        <script>
+            //App custom javascript
+        </script>
+    </body>
+</html>

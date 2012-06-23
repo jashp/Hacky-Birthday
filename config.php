724e743997fd18fef8378fa520ae1f9d1b454a4b
Jash Patel <jashpp@gmail.com>
2012-06-23 01:17:14 -0400

    Updated FB files, redesigned home page using Jquery mobile, centralized FB settings to config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	include_once('php-sdk/facebook.php');
+	date_default_timezone_set('UTC');
+	$app_id = "152916161509664";
+	$app_secret = "673c3da0f16c3a6fd357dda1e6cdfffc";
+	$app_namespace = "hbd-fbhack";
+	$facebook = new Facebook( array( 'appId' => $app_id, 'secret' => $app_secret, ));
+	$login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_actions,user_birthday,user_likes,friends_birthday,friends_likes,publish_stream,friends_photos') );
+?>
--- index.php
+++ index.php
@@ -1,66 +1,62 @@
 <?php
-include_once('php-sdk/facebook.php');
-
-if (isset($_REQUEST['username'])) {
-  $username = $_REQUEST['username'];
-}
-
-
-
-date_default_timezone_set('UTC');
-$app_url = "https://csclub.uwaterloo.ca/~y5pei"; // no slash at the end, e.g. 'https://social-cafe.herokuapp.com'
-$app_id = "152916161509664";
-$app_secret = "673c3da0f16c3a6fd357dda1e6cdfffc";
-$app_namespace = "hbd-fbhack"; // no colon at the end, e.g. 'social-cafe'
-
-
-$facebook = new Facebook( array(
-                           'appId' => $app_id,
-                           'secret' => $app_secret,
-                         ));
-  
-  $login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_actions,user_birthday,user_likes,friends_birthday,friends_relationships,friends_likes,publish_stream,friends_photos') );
-
-    if(!$facebook->getUser()) {
-     echo '<a href="' . $login_url . '">Login Testing</a>';
-   } else {
-      
-			  // if($_GET["at"]) {
-				// echo '<p><pre>access token: ' . $facebook->getAccessToken() . '</pre></p>';
-			  // }
-			  
-			  // $me = $facebook->api("/me",'GET');
-			  // $me['likes'] = $facebook->api("/me/likes", 'GET');
-			  $me['friends'] = $facebook->api('/me/friends?fields=id,name,birthday', 'GET');
-
-				  // get todays date
-				  $today = "06/17"; //date("m/d"); Hard coding date for testing purposes.
-			  $birthdayppl = array();
-
-			  foreach($me['friends']['data'] as $person){
-				if(isset($person['birthday']) && substr($person['birthday'], 0, 5) == $today){
-				  $birthdayppl[] = $person;
-				}
-			  }
-
-        
-			  foreach($birthdayppl as $b) { 
-				?>
-				<div class="row">
-					
-						<div class="span3"> 
-						  <?php echo $b['name'] ?>
-						</div>
-						<div class="span3">
-						  <div class="btn-group">
-							<form method= "post" action="person.php?<?php echo 'id='.$b['id']; ?>">
-							<button class="btn">Say Happy Birthday!</button>
-							</form>
-						  </div>
-						</div>
-					
-				</div>
-				<?php
-			  }
+	include_once('config.php');
+	
+	$loggedIn = $facebook->getUser();
+	if($loggedIn) {
+		$friends = $facebook->api('/me/friends?fields=id,name,birthday', 'GET');
+		$today = "06/16";
+		//date("m/d"); Hard coding date for testing purposes.
+		$birthdayppl = array();
+		foreach($friends['data'] as $person){
+			if(isset($person['birthday']) && substr($person['birthday'], 0, 5) == $today){
+				$birthdayppl[] = $person;
+			}
+		}
 	}
-?>      
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>
+        </title>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />
+        <link rel="stylesheet" href="my.css" />
+        <style>
+            /* App custom styles */
+        </style>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+        </script>
+        <script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js">
+        </script>
+    </head>
+    <body>
+        <!-- Home -->
+		<div data-role="page" id="page1">
+            <div data-theme="a" data-role="header">
+                <h3>
+                    Hacky Birthday!
+                </h3>
+            </div>
+            <div data-role="content" style="padding: 15px">
+                <ul data-role="listview" data-divider-theme="a" data-inset="true">
+                    <li data-role="list-divider" role="heading">
+                        Birthdays Today
+                    </li>
+					<?php foreach($birthdayppl as $b) { ?>
+						<li data-theme="c">
+							<a href="person.php?id=<?php echo $b['id']; ?>" data-transition="slide">
+								<?php echo $b['name']; ?>
+							</a>
+						</li>
+					<?php } ?>
+                </ul>
+            </div>
+        </div>
+        <script>
+            //App custom javascript
+        </script>
+    </body>
+</html>

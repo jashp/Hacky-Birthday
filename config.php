04500b551e0d43eed312f29edcca97fbac35cc1e
Jash Patel <jashpp@gmail.com>
2012-06-29 22:10:34 -0400

    Ignore config.php
    Keep app secret, etc private

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	include_once('php-sdk/facebook.php');
 	date_default_timezone_set('America/Toronto');
-	$app_id = "000000000000000";
-	$app_secret = "00000000000000000000000000000000";
-	$app_namespace = "APP-NAME-SPACE";
+	$app_id = "152916161509664";
+	$app_secret = "84b3dca9821b32d86eaf192b288d78a3";
+	$app_namespace = "hacky-birthday";
 	$facebook = new Facebook( array( 'appId' => $app_id, 'secret' => $app_secret, ));
 	function getLoginUrl(){
 		global $facebook;
